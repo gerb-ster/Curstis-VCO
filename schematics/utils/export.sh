fd30e8982044e733bafa3b188a1b03c59078096b
Gerbrand Sterrenburg <gerb@gerbster.nl>
2022-05-21 22:08:06 +0200

    Fixed schematics

--- schematics/utils/export.sh
+++ schematics/utils/export.sh
@@ -7,6 +7,8 @@ NOCOLOR="\033[0m"
 
 source ../../.project
 
+rm -Rf ../../schematics/*.pdf
+
 for FOLDER in ../src/*; do
     # if this is a folder, continue
     if [ -d "$FOLDER" ]; then
@@ -14,6 +16,7 @@ for FOLDER in ../src/*; do
         if [[ -f "$FOLDER/eagle.epf" ]]; then
             FOLDER_NAME=`basename "$FOLDER"`
             BRD_FILE=$FOLDER/$FOLDER_NAME.brd
+            SCH_FILE=$FOLDER/$FOLDER_NAME.sch
 
             echo -e "${BLUE}Found a Eagle Project in $FOLDER for $PROJECT_NAME${NOCOLOR}"
 
@@ -26,14 +29,15 @@ for FOLDER in ../src/*; do
 
                 # run cam job & export to tmp folder and name them properly
                 mkdir ./tmp
-                eagle -X -dCAMJOB -j./jlcpcb_2_layer_v9.cam -o./tmp $BRD_FILE > /dev/null 2>&1
+                eagle -X -d CAMJOB -j ./jlcpcb_2_layer_v9.cam -o ./tmp $BRD_FILE > /dev/null 2>&1
+                eagle -C "PRINT landscape 1.0 -0 -caption FILE '../$PROJECT_NAME-$FOLDER_NAME.pdf' sheets all paper a3; QUIT;" $SCH_FILE > /dev/null 2>&1;
                 mv ./tmp/tmp/* ./tmp/
                 rm -Rf ./tmp/tmp
                 cd ./tmp
                 for f in * ; do mv -- "$f" "$PROJECT_NAME-$f" ; done
                 cd ../
             
-                echo -e "${BLUE}Created Gerber Files for $PROJECT_NAME${NOCOLOR}"
+                echo -e "${BLUE}Created Gerber Files & PDF for $PROJECT_NAME${NOCOLOR}"
 
                 # move BOM file to proper directory
                 mv ./tmp/$PROJECT_NAME-BOM.html ../BOM/$PROJECT_NAME-$FOLDER_NAME.html
